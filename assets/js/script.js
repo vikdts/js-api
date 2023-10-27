83868af21817f6ed987bbfae32704c6cc82bc81b
vikdts <>
2023-10-27 06:59:30 +0000

    Make a post request to the api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,23 @@
 const API_KEY = "-LKwAiaO8YvterqqvFR2Nck9hLM";
 const API_URL = "https://ci-jshint.herokuapp.com/api";
-const resultModal = new bootstrap.Modal(document.getElementById("resultsModal"));
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
 document.getElementById("status").addEventListener("click", e => getStatus(e));
+document.getElementById("submit").addEventListener("click", e => postForm(e));
+
+async function postForm(e) {
+
+    const form = new FormData(document.getElementById("checksform"));
+
+    const response = await fetch(API_URL, {
+        method: "POST",
+        headers: {
+            "Authorization": API_KEY,
+        },
+        body: form,
+    });
+
+}
 
 async function getStatus(e) {
 
@@ -14,7 +29,6 @@ async function getStatus(e) {
 
     if (response.ok) {
         displayStatus(data);
-
     } else {
         throw new Error(data.error);
     }
